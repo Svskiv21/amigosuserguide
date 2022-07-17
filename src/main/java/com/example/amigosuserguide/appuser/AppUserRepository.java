55be1d5a9a4390cb20f104324926b801c2576e9c
Svskiv21 <natalia.krukar@gmail.com>
2022-07-17 19:00:15 +0200

    added new security classes + fixed application.yml, everything works

--- src/main/java/com/example/amigosuserguide/appuser/AppUserRepository.java
+++ src/main/java/com/example/amigosuserguide/appuser/AppUserRepository.java
@@ -1,6 +1,8 @@
 package com.example.amigosuserguide.appuser;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,6 +10,7 @@ import java.util.Optional;
 
 @Repository
 @Transactional(readOnly = true)
-public interface AppUserRepository {
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
     Optional<AppUser> findByEmail(String email);
 }
