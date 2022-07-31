dbaaaad4fc91e618cfbed814e27923b43336d972
Svskiv21 <natalia.krukar@gmail.com>
2022-07-31 12:23:34 +0200

    registrating user should works by now but it returns html site instead of message from registration method

---
+++ README.md
@@ -0,0 +1,6 @@
+database setup
+1. login
+2. \l to show all existing databases
+3. \c <database_name> to connect to certain db
+4. \d to show all tables
+5. \d <table_name> to see all rows with types
--- src/main/java/com/example/amigosuserguide/appuser/AppUser.java
+++ src/main/java/com/example/amigosuserguide/appuser/AppUser.java
@@ -19,38 +19,36 @@ import java.util.Collections;
 @Entity
 public class AppUser implements UserDetails {
 
-    @Id
     @SequenceGenerator(
             name = "student_sequence",
             sequenceName = "student_sequence",
-            allocationSize = 1)
+            allocationSize = 1
+    )
+    @Id
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
-            generator = "student_sequence")
+            generator = "student_sequence"
+    )
     private Long id;
-    private String name;
-    private String username;
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
-    private Boolean locked;
-    private Boolean enabled;
+    private Boolean locked = false;
+    private Boolean enabled = false;
 
-    public AppUser(String name,
-                   String username,
+    public AppUser(String firstName,
+                   String lastName,
                    String email,
                    String password,
-                   AppUserRole appUserRole,
-                   Boolean locked,
-                   Boolean enabled) {
-        this.name = name;
-        this.username = username;
+                   AppUserRole appUserRole) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.appUserRole = appUserRole;
-        this.locked = locked;
-        this.enabled = enabled;
     }
 
     @Override
@@ -67,7 +65,15 @@ public class AppUser implements UserDetails {
 
     @Override
     public String getUsername() {
-        return username;
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 
     @Override
